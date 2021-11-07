b8509cb8b7641efb04627ff0831ba57514b8a765
Farida <faridamus@protonmail.com>
2021-11-07 01:40:14 +0000

    Complete and lint

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
-
-  commit() {
-    balance -= this.amount;
-  }
-
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance += this.value;
+  }
+}
 
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("Farida");
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(123.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(34.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(12.00, myAccount);
+t3.commit();
+
+console.log('Ending Balance:', myAccount.balance);
